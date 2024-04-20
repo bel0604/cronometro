f494f3baebd893e97cdf10c4a6ddc52608b23b10
Alexandre_oliveiradev <alexandredev.frontend@gmail.com>
2024-04-20 11:29:43 -0300

    Primeiro commit do cronometro

--- Cronometro/index.js
+++ Cronometro/index.js
@@ -1,18 +1,43 @@
-function iniciar (){
-    var seg = 0;
-    var min = 0;
-    var hora = 0;
+var seg = 0;
+var min = 0;
+var hora = 0;
+let interval
 
-    setInterval(() => {
+const display = document.getElementById('display')
+
+function iniciar (){
+   interval = setInterval(() => {
         seg ++;
         if (seg == 60){
         seg = 0;
         min ++;
-        }if( min == 60){
+        }
+       
+       if (min == 60) {
             min = 0;
             hora ++
         }
+       
+       // TERNÁRIO: CONDIÇÃO ? FAZ ALGUMA COISA : FAZ OUTRA COISA 
+       /* 
+       ? = ENTÃO
+       : = SE NÃO
+       */
+       display.innerHTML = `${hora < 10 ? '0' + hora : hora }: ${min < 10 ? '0' + min : min }:${seg < 10 ? '0' + seg : seg }`
         console.log(`${hora}:${min}:${seg}`);
     },1000);
 }
 
+
+function pause() {
+    clearInterval(interval)
+}
+
+function reset() {
+    seg = 0
+    min = 0
+    hora = 0
+    display.innerHTML = `${hora < 10 ? '0' + hora : hora }: ${min < 10 ? '0' + min : min }:${seg < 10 ? '0' + seg : seg }`
+    pause()
+    console.log(`${hora}:${min}:${seg}`);
+}
